5b8ef5b7bd6cabfd6df79e0bc00eb8355c8d11f2
kitaharafay <1360019840@qq.com>
2019-08-01 00:09:34 +0800

    重写了task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,34 +1,42 @@
 var gulp = require('gulp');
+var less = require('gulp-less')
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
-var less = require('gulp-less');
 var minifyCSS = require('gulp-cssnano');
 var prefix = require('gulp-autoprefixer');
-
-gulp.task('copy', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('script', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe(uglify())
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('dist'));
-});
+var concat = require('gulp-concat');
+var jslint = require('gulp-jslint');
+var del = require('del');
 
 gulp.task('styles', function () {
-  return gulp.src('app/styles/main.less')
-    .pipe(less())
-    .pipe(minifyCSS())
-    .pipe(prefix())
-    .pipe(gulp.dest('dist/styles'));
+    return gulp.src('app/styles/main.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(prefix())
+        .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('test', function(){
-  return gulp.src('app/scripts/**/*.js', '!app/scripts/vendor/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
+gulp.task('test', function () {
+    return gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'])
+        .pipe(jslint());
 });
+
+gulp.task('clean', function () {
+    return del(['dist']);
+});
+
+gulp.task('report', function (done) {
+    console.log('We are done!');
+    done();
+});
+
+gulp.task('scripts',
+    gulp.series('test', function srcriptInternal() {
+        return gulp.src(['app/scripts/vendor/**/*.js', 'app/scripts/**/*.js'])
+            .pipe(concat('main.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('dist'));
+    }));
+
+gulp.task('default',
+    gulp.series('clean',
+        gulp.parallel('styles', 'scripts')));
